5ebb05654978004eac6076eda86ce67e1d536487
Vyacheslav <kirillov.com@gmail.com>
2023-05-07 20:48:31 +0300

    Initial commit

---
+++ src/main/java/com/example/ideaautoservlet/model/User.java
@@ -0,0 +1,51 @@
+package com.example.mytestservlet.model;
+
+
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthDay;
+    private String company;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
